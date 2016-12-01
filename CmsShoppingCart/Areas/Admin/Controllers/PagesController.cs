46b0ca5481ff5f96a2d613cadf353fd8f1e81ebf
Voja <Voja@DESKTOP-2KA0NFF>
2016-12-01 18:35:52 +0100

    admin pages 4

--- CmsShoppingCart/Areas/Admin/Controllers/PagesController.cs
+++ CmsShoppingCart/Areas/Admin/Controllers/PagesController.cs
@@ -27,9 +27,66 @@ namespace CmsShoppingCart.Areas.Admin.Controllers
         }
 
         // GET: Admin/Pages/AddPage
+        [HttpGet]
         public ActionResult AddPage()
         {
             return View();
+        }
+
+        // POST: Admin/Pages/AddPage
+        [HttpPost]
+        public ActionResult AddPage(PageVM model)
+        {
+            // Check model state
+            if (! ModelState.IsValid)
+            {
+                return View(model);
+            }
+
+            using (Db db = new Db())
+            {
+                // Declare slug
+                string slug;
+
+                // Init pageDTO
+                PageDTO dto = new PageDTO();
+
+                // DTO title
+                dto.Title = model.Title;
+
+                // Check for and set slug if need be
+                if (string.IsNullOrWhiteSpace(model.Slug))
+                {
+                    slug = model.Title.Replace(" ", "-").ToLower();
+                }
+                else
+                {
+                    slug = model.Slug.Replace(" ", "-").ToLower();
+                }
+
+                // Make sure title and slug are unique
+                if ( db.Pages.Any(x => x.Title == model.Title) || db.Pages.Any(x => x.Slug == slug) )
+                {
+                    ModelState.AddModelError("", "That title or slug already exists.");
+                    return View(model);
+                }
+
+                // DTO the rest
+                dto.Slug = slug;
+                dto.Body = model.Body;
+                dto.HasSidebar = model.HasSidebar;
+                dto.Sorting = 100;
+
+                // Save DTO
+                db.Pages.Add(dto);
+                db.SaveChanges();
+            }
+
+            // Set TempData message
+            TempData["SM"] = "You have added a new page!";
+
+            // Redirect
+            return RedirectToAction("AddPage");
         }
     }
 }
