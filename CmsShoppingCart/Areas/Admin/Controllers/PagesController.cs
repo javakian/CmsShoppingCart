c5b0e8b3a4193eb64c11263f87d3ec92a1441b91
Voja <Voja@DESKTOP-2KA0NFF>
2016-12-01 20:07:30 +0100

    admin pages 8

--- CmsShoppingCart/Areas/Admin/Controllers/PagesController.cs
+++ CmsShoppingCart/Areas/Admin/Controllers/PagesController.cs
@@ -200,5 +200,24 @@ namespace CmsShoppingCart.Areas.Admin.Controllers
             // Return view with model
             return View(model);
         }
+
+        // GET: Admin/Pages/DeletePage/id
+        public ActionResult DeletePage(int id)
+        {
+            using (Db db = new Db())
+            {
+                // Get the page
+                PageDTO dto = db.Pages.Find(id);
+
+                // Remove the page
+                db.Pages.Remove(dto);
+
+                // Save
+                db.SaveChanges();
+            }
+
+            // Redirect
+            return RedirectToAction("Index");
+        }
     }
 }
