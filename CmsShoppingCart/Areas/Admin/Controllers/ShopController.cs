05537c055ef3a2f80774010b1adc27b3cf74be62
Voja <Voja@DESKTOP-2KA0NFF>
2016-12-03 12:29:09 +0100

    admin shop 6

--- CmsShoppingCart/Areas/Admin/Controllers/ShopController.cs
+++ CmsShoppingCart/Areas/Admin/Controllers/ShopController.cs
@@ -62,5 +62,50 @@ namespace CmsShoppingCart.Areas.Admin.Controllers
             // Return id
             return id;
         }
+
+        // POST: Admin/Shop/ReorderCategories
+        [HttpPost]
+        public void ReorderCategories(int[] id)
+        {
+            using (Db db = new Db())
+            {
+                // Set initial count
+                int count = 1;
+
+                // Declare CategoryDTO
+                CategoryDTO dto;
+
+                // Set sorting for each category
+                foreach (var catId in id)
+                {
+                    dto = db.Categories.Find(catId);
+                    dto.Sorting = count;
+
+                    db.SaveChanges();
+
+                    count++;
+                }
+            }
+
+        }
+
+        // GET: Admin/Shop/DeleteCategory/id
+        public ActionResult DeleteCategory(int id)
+        {
+            using (Db db = new Db())
+            {
+                // Get the category
+                CategoryDTO dto = db.Categories.Find(id);
+
+                // Remove the category
+                db.Categories.Remove(dto);
+
+                // Save
+                db.SaveChanges();
+            }
+
+            // Redirect
+            return RedirectToAction("Categories");
+        }
     }
 }
