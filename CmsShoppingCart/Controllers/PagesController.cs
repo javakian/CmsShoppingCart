d1a67f92fae450354f75ed0b6cac19497d52fd15
Voja <Voja@DESKTOP-2KA0NFF>
2016-12-06 12:53:32 +0100

    front pages 1

---
+++ CmsShoppingCart/Controllers/PagesController.cs
@@ -0,0 +1,59 @@
+﻿using CmsShoppingCart.Models.Data;
+using CmsShoppingCart.Models.ViewModels.Pages;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CmsShoppingCart.Controllers
+{
+    public class PagesController : Controller
+    {
+        // GET: Index/{page}
+        public ActionResult Index(string page = "")
+        {
+            // Get/set page slug
+            if (page == "")
+                page = "home";
+
+            // Declare model and DTO
+            PageVM model;
+            PageDTO dto;
+
+            // Check if page exists
+            using (Db db = new Db())
+            {
+                if (! db.Pages.Any(x => x.Slug.Equals(page)))
+                {
+                    return RedirectToAction("Index", new { page = "" });
+                }
+            }
+
+            // Get page DTO
+            using (Db db = new Db())
+            {
+                dto = db.Pages.Where(x => x.Slug == page).FirstOrDefault();
+            }
+
+            // Set page title
+            ViewBag.PageTitle = dto.Title;
+
+            // Check for sidebar
+            if (dto.HasSidebar == true)
+            {
+                ViewBag.Sidebar = "Yes";
+            }
+            else
+            {
+                ViewBag.Sidebar = "No";
+            }
+
+            // Init model
+            model = new PageVM(dto);
+
+            // Return view with model
+            return View(model);
+        }
+    }
+}
