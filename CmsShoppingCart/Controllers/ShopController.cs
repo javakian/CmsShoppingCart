e8df8939cbc6a9986aa82c24be21a770b125b897
Voja <Voja@DESKTOP-2KA0NFF>
2016-12-06 18:32:48 +0100

    front shop 3

--- CmsShoppingCart/Controllers/ShopController.cs
+++ CmsShoppingCart/Controllers/ShopController.cs
@@ -2,6 +2,7 @@
 using CmsShoppingCart.Models.ViewModels.Shop;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -53,6 +54,43 @@ namespace CmsShoppingCart.Controllers
 
             // Return view with list
             return View(productVMList);
+        }
+
+        // GET: /shop/product-details/name
+        [ActionName("product-details")]
+        public ActionResult ProductDetails(string name)
+        {
+            // Declare the VM and DTO
+            ProductVM model;
+            ProductDTO dto;
+
+            // Init product id
+            int id = 0;
+
+            using (Db db = new Db())
+            {
+                // Check if product exists
+                if (! db.Products.Any(x => x.Slug.Equals(name)))
+                {
+                    return RedirectToAction("Index", "Shop");
+                }
+
+                // Init productDTO
+                dto = db.Products.Where(x => x.Slug == name).FirstOrDefault();
+
+                // Get id
+                id = dto.Id;
+
+                // Init model
+                model = new ProductVM(dto);
+            }
+
+            // Get gallery images
+            model.GalleryImages = Directory.EnumerateFiles(Server.MapPath("~/Images/Uploads/Products/" + id + "/Gallery/Thumbs"))
+                                                .Select(fn => Path.GetFileName(fn));
+
+            // Return view with model
+            return View("ProductDetails", model);
         }
     }
 }
