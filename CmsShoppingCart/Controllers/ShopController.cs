14f54c716329c1f352dfeeaf1ee1b85fc9d5eb00
Voja <Voja@DESKTOP-2KA0NFF>
2016-12-06 17:55:37 +0100

    shop front 2

--- CmsShoppingCart/Controllers/ShopController.cs
+++ CmsShoppingCart/Controllers/ShopController.cs
@@ -30,5 +30,29 @@ namespace CmsShoppingCart.Controllers
             // Return partial with list
             return PartialView(categoryVMList);
         }
+
+        // GET: /shop/category/name
+        public ActionResult Category(string name)
+        {
+            // Declare a list of ProductVM
+            List<ProductVM> productVMList;
+
+            using (Db db = new Db())
+            {
+                // Get category id
+                CategoryDTO categoryDTO = db.Categories.Where(x => x.Slug == name).FirstOrDefault();
+                int catId = categoryDTO.Id;
+
+                // Init the list
+                productVMList = db.Products.ToArray().Where(x => x.CategoryId == catId).Select(x => new ProductVM(x)).ToList();
+
+                // Get category name
+                var productCat = db.Products.Where(x => x.CategoryId == catId).FirstOrDefault();
+                ViewBag.CategoryName = productCat.CategoryName;
+            }
+
+            // Return view with list
+            return View(productVMList);
+        }
     }
 }
