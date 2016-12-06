7a7004cd25a6e2dcee48ecd8bcd78de117be4f46
Voja <Voja@DESKTOP-2KA0NFF>
2016-12-06 16:57:14 +0100

    shop front 1

--- CmsShoppingCart/App_Start/RouteConfig.cs
+++ CmsShoppingCart/App_Start/RouteConfig.cs
@@ -13,6 +13,8 @@ namespace CmsShoppingCart
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
+            routes.MapRoute("Shop", "Shop/{action}/{name}", new { controller = "Shop", action = "Index", name = UrlParameter.Optional }, new[] { "CmsShoppingCart.Controllers" });
+
             routes.MapRoute("SidebarPartial", "Pages/SidebarPartial", new { controller = "Pages", action = "SidebarPartial" }, new[] { "CmsShoppingCart.Controllers" });
             routes.MapRoute("PagesMenuPartial", "Pages/PagesMenuPartial", new { controller = "Pages", action = "PagesMenuPartial" }, new[] { "CmsShoppingCart.Controllers" });
             routes.MapRoute("Pages", "{page}", new { controller = "Pages", action = "Index" }, new[] { "CmsShoppingCart.Controllers" });
---
+++ CmsShoppingCart/Controllers/ShopController.cs
@@ -0,0 +1,34 @@
+﻿using CmsShoppingCart.Models.Data;
+using CmsShoppingCart.Models.ViewModels.Shop;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CmsShoppingCart.Controllers
+{
+    public class ShopController : Controller
+    {
+        // GET: Shop
+        public ActionResult Index()
+        {
+            return RedirectToAction("Index", "Pages");
+        }
+
+        public ActionResult CategoryMenuPartial()
+        {
+            // Declare list of CategoryVM
+            List<CategoryVM> categoryVMList;
+
+            // Init the list
+            using (Db db = new Db())
+            {
+                categoryVMList = db.Categories.ToArray().OrderBy(x => x.Sorting).Select(x => new CategoryVM(x)).ToList();
+            }
+
+            // Return partial with list
+            return PartialView(categoryVMList);
+        }
+    }
+}
