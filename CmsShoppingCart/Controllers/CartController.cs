950ca4739f63ea45938bbfcd85fc9638f94a4128
Voja <Voja@DESKTOP-2KA0NFF>
2016-12-06 23:27:25 +0100

    front cart 3

--- CmsShoppingCart/Controllers/CartController.cs
+++ CmsShoppingCart/Controllers/CartController.cs
@@ -1,4 +1,5 @@
-﻿using CmsShoppingCart.Models.ViewModels.Cart;
+﻿using CmsShoppingCart.Models.Data;
+using CmsShoppingCart.Models.ViewModels.Cart;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -67,6 +68,62 @@ namespace CmsShoppingCart.Controllers
                 model.Quantity = 0;
                 model.Price = 0m;
             }
+
+            // Return partial view with model
+            return PartialView(model);
+        }
+
+        public ActionResult AddToCartPartial(int id)
+        {
+            // Init CartVM list
+            List<CartVM> cart = Session["cart"] as List<CartVM> ?? new List<CartVM>();
+
+            // Init CartVM
+            CartVM model = new CartVM();
+
+            using (Db db = new Db())
+            {
+                // Get the product
+                ProductDTO product = db.Products.Find(id);
+
+                // Check if the product is already in cart
+                var productInCart = cart.FirstOrDefault(x => x.ProductId == id);
+
+                // If not, add new
+                if (productInCart == null)
+                {
+                    cart.Add(new CartVM()
+                    {
+                        ProductId = product.Id,
+                        ProductName = product.Name,
+                        Quantity = 1,
+                        Price = product.Price,
+                        Image = product.ImageName
+                    });
+                }
+                else
+                {
+                    // If it is, increment
+                    productInCart.Quantity++;
+                }
+            }
+
+            // Get total qty and price and add to model
+
+            int qty = 0;
+            decimal price = 0m;
+
+            foreach (var item in cart)
+            {
+                qty += item.Quantity;
+                price += item.Quantity * item.Price;
+            }
+
+            model.Quantity = qty;
+            model.Price = price;
+
+            // Save cart back to session
+            Session["cart"] = cart;
 
             // Return partial view with model
             return PartialView(model);
