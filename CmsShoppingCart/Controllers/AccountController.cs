17424e7699af11a297d88ebe83552d4466e32c33
Voja <Voja@DESKTOP-2KA0NFF>
2016-12-07 18:28:30 +0100

    account 4

--- CmsShoppingCart/Controllers/AccountController.cs
+++ CmsShoppingCart/Controllers/AccountController.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using CmsShoppingCart.Models.Data;
+using CmsShoppingCart.Models.ViewModels.Account;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -30,9 +32,74 @@ namespace CmsShoppingCart.Controllers
 
         // GET: /account/create-account
         [ActionName("create-account")]
+        [HttpGet]
         public ActionResult CreateAccount()
         {
             return View("CreateAccount");
+        }
+
+        // POST: /account/create-account
+        [ActionName("create-account")]
+        [HttpPost]
+        public ActionResult CreateAccount(UserVM model)
+        {
+            // Check model state
+            if (!ModelState.IsValid)
+            {
+                return View("CreateAccount", model);
+            }
+
+            // Check if passwords match
+            if (!model.Password.Equals(model.ConfirmPassword))
+            {
+                ModelState.AddModelError("", "Passwords do not match.");
+                return View("CreateAccount", model);
+            }
+
+            using (Db db = new Db())
+            {
+                // Make sure username is unique
+                if (db.Users.Any(x => x.Username.Equals(model.Username)))
+                {
+                    ModelState.AddModelError("", "Username " + model.Username + " is taken.");
+                    model.Username = "";
+                    return View("CreateAccount", model);
+                }
+
+                // Create userDTO
+                UserDTO userDTO = new UserDTO()
+                {
+                    FirstName = model.FirstName,
+                    LastName = model.LastName,
+                    EmailAddress = model.EmailAddress,
+                    Username = model.Username,
+                    Password = model.Password
+                };
+
+                // Add the DTO
+                db.Users.Add(userDTO);
+
+                // Save
+                db.SaveChanges();
+
+                // Add to UserRolesDTO
+                int id = userDTO.Id;
+
+                UserRoleDTO userRolesDTO = new UserRoleDTO()
+                {
+                    UserId = id,
+                    RoleId = 2
+                };
+
+                db.UserRoles.Add(userRolesDTO);
+                db.SaveChanges();
+            }
+
+            // Create a TempData message
+            TempData["SM"] = "You are now registered and can login.";
+
+            // Redirect
+            return Redirect("~/account/login");
         }
     }
 }
