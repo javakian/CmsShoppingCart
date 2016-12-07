9fab22e6a736c240f980caf21ed2f64dbf44d35c
Voja <Voja@DESKTOP-2KA0NFF>
2016-12-07 20:09:26 +0100

    account 7

--- CmsShoppingCart/Controllers/AccountController.cs
+++ CmsShoppingCart/Controllers/AccountController.cs
@@ -169,5 +169,29 @@ namespace CmsShoppingCart.Controllers
             // Return partial view with model
             return PartialView(model);
         }
+
+        // GET: /account/user-profile
+        [HttpGet]
+        [ActionName("user-profile")]
+        public ActionResult UserProfile()
+        {
+            // Get username
+            string username = User.Identity.Name;
+
+            // Declare model
+            UserProfileVM model;
+
+            using (Db db = new Db())
+            {
+                // Get user
+                UserDTO dto = db.Users.FirstOrDefault(x => x.Username == username);
+
+                // Build model
+                model = new UserProfileVM(dto);
+            }
+
+            // Return view with model
+            return View("UserProfile", model);
+        }
     }
 }
