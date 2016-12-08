fad8004f2cc57669e370319538b4d733ed707919
Voja <Voja@DESKTOP-2KA0NFF>
2016-12-08 12:05:39 +0100

    place order 1

--- CmsShoppingCart/Models/Data/Db.cs
+++ CmsShoppingCart/Models/Data/Db.cs
@@ -15,5 +15,7 @@ namespace CmsShoppingCart.Models.Data
         public DbSet<UserDTO> Users { get; set; }
         public DbSet<RoleDTO> Roles { get; set; }
         public DbSet<UserRoleDTO> UserRoles { get; set; }
+        public DbSet<OrderDTO> Orders { get; set; }
+        public DbSet<OrderDetailsDTO> OrderDetails { get; set; }
     }
 }
