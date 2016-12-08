b83a07d27da037843fd46f00af02179f1479cd51
Voja <Voja@DESKTOP-2KA0NFF>
2016-12-08 10:46:57 +0100

    roles

--- CmsShoppingCart/Global.asax.cs
+++ CmsShoppingCart/Global.asax.cs
@@ -1,6 +1,8 @@
-﻿using System;
+﻿using CmsShoppingCart.Models.Data;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Principal;
 using System.Web;
 using System.Web.Mvc;
 using System.Web.Optimization;
@@ -16,6 +18,33 @@ namespace CmsShoppingCart
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+        }
+
+        protected void Application_AuthenticateRequest()
+        {
+            // Check if user is logged in
+            if (User == null) { return; }
+
+            // Get username
+            string username = Context.User.Identity.Name;
+
+            // Declare array of roles
+            string[] roles = null;
+
+            using (Db db = new Db())
+            {
+                // Populate roles
+                UserDTO dto = db.Users.FirstOrDefault(x => x.Username == username);
+
+                roles = db.UserRoles.Where(x => x.UserId == dto.Id).Select(x => x.Role.Name).ToArray();
+            }
+
+            // Build IPrincipal object
+            IIdentity userIdentity = new GenericIdentity(username);
+            IPrincipal newUserObj = new GenericPrincipal(userIdentity, roles);
+
+            // Update Context.User
+            Context.User = newUserObj;
         }
     }
 }
