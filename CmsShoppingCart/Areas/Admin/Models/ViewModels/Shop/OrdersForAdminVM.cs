f700744c8e63ad66c5eac6738d40086d63ae781d
Voja <Voja@DESKTOP-2KA0NFF>
2016-12-08 18:43:31 +0100

    66 order vms

---
+++ CmsShoppingCart/Areas/Admin/Models/ViewModels/Shop/OrdersForAdminVM.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CmsShoppingCart.Areas.Admin.Models.ViewModels.Shop
+{
+    public class OrdersForAdminVM
+    {
+        public int OrderNumber { get; set; }
+        public string Username { get; set; }
+        public decimal Total { get; set; }
+        public Dictionary<string, int> ProductsAndQty { get; set; }
+        public DateTime CreatedAt { get; set; }
+    }
+}
